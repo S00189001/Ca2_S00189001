c7c9db960c4c14c21b413be727fb07c835536c21
Matthew Bowen <46889006+S00189001@users.noreply.github.com>
2019-12-02 16:43:48 +0000

    Added More Comments and Cleaned Code

--- Activity.cs
+++ Activity.cs
@@ -29,18 +29,20 @@ namespace CA2_S00189001
             description = DESCRIPTION;
         }
 
+        // CompareTo is a method used for sorting lists.
         public int CompareTo(object obj)
         {
             Activity activity = obj as Activity;
             return activity.date.CompareTo(this.date);
         }
 
+        // Formatting the strings within the list boxes. :)
         public override string ToString()
         {
             return Name + " - " + date.ToShortDateString();
         }
 
-
+        // Will display the description of the selected activity along with the formatted cost.
         public string GetDescription()
         {
             return String.Format("{0} - {1:c}", description, cost);
